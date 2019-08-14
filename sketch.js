cd2cf931149ef5c5828f92ce29de292497f1fb60
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-14 01:48:15 -0400

    Add images

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
+let img;
+let img2;
+function preload() {
+  img = loadImage('assets/what_are_we.png');
+  img2 = loadImage('assets/worthwhile.png');
+}
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  imageMode(CENTER);
 }
 
 function draw() {
   background(255);
+
+  if(mouseIsPressed) {
+    image(img2, mouseX, mouseY);
+  } else {
+    image(img, mouseX, mouseY);
+  }
+
   for (var x = 0; x <= width; x += 51) {
     for (var y = 0; y <= height; y += 51) {
       let newX = random(255);
